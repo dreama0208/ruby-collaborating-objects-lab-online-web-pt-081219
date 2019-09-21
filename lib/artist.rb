04d597e94efa4e41bae8b181d694c76a7eb3ed10
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-21 13:57:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,6 +14,7 @@ class Artist
     @@all << self
     self
   end
+
   def self.all
     @@all
   end
@@ -29,9 +30,9 @@ class Artist
 
   def self.find_or_create_by_name(name)
     if(self.name.nil?)
-      Artist.new(name)
+      Artist.new(name).save
     else
-      self.all.find {|artist| artist.name == name}
+      self.all.detect {|artist| artist.name == name}
     end
     self
   end
